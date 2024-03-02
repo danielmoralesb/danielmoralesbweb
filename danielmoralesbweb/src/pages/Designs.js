6638da13a2d470da622d7f60be549502915f53f7
daniel morales <daniel_morales81@hotmail.commorales81@hotmail.com>
2024-03-02 18:54:34 -0500

    adding router navigation and rest of the pages also components header and foter

---
+++ danielmoralesbweb/src/pages/Designs.js
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function NoPage() {
+    return(
+        <div>
+            <div className="container">
+                <Header />
+                <p>Designs</p>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default NoPage;
---
+++ danielmoralesbweb/src/pages/Home.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function Home() {
+    return(
+        <div>
+            <div className="container">
+                <Header />
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
