e79ed8169abf66e07660ba68037d96b4da1f0059
daniel morales <daniel_morales81@hotmail.commorales81@hotmail.com>
2024-04-28 19:25:08 -0400

    updating case studies and background images for the body tag

--- danielmoralesbweb/src/components/SectionList.jsx
+++ danielmoralesbweb/src/components/SectionList.jsx
@@ -1,21 +1,39 @@
+import React, { useState } from "react";
+
 import { data } from "../data";
 
 function SectionList(props) {
+    let [toggleContent, setToggleContent] = useState(false);
+
+    const toggleThis =(index) => {
+        setToggleContent(prevState => ({
+            ...prevState,
+            [index]: !prevState[index]
+        }));
+    }
     return (
         <>
             {
                 data.sectionList[props.page].map((section, index) => (
-                    <section key={index} className="project">
+                    <section key={index} className="project project--list">
                         <div className="project__inner container">
                             <div className="project__text">
                                 <h3 className="project__title">{section.title}</h3>
                                 <p className="project__date">{section.date}</p>
                                 <p className="project__description">{section.description}</p>
-                                <p className="project__link"><a href={section.url} title={section.title} target="_blank" rel="noreferrer">View site</a></p>
+                                <p className="project__link"><button className="btn btn--link" onClick={() => toggleThis(index)}>View case</button></p>
                             </div>
                             <div className="project__image">
-                                <img src={section.image} alt={section.title} />
+                                <img src={section.image} alt={section.imageAlt} />
                             </div>
+                        </div>
+                        <div className={`project__content container ${toggleContent[index] ? "show" : ""}`}>
+                            <h4>Challenge</h4>
+                            <p>{section.challenge}</p>
+                            <h4>Approach</h4>
+                            <p>{section.approach}</p>
+                            <h4>Results</h4>
+                            <p>{section.results}</p>
                         </div>
                     </section>
                 ))
--- danielmoralesbweb/src/components/Slider.jsx
+++ danielmoralesbweb/src/components/Slider.jsx
@@ -47,29 +47,6 @@ const Slider = () => {
             </section>
         </div>
     );
-
-    // return (
-    //     <div className="container">
-    //         <section className="testimonials">
-    //             <h2>Testimonials</h2>
-    //             <div className="slider-text">
-    //                 <div className='slider-text__inner'>
-    //                     <q>{data.testimonials[currentSlide].content}</q>
-    //                     <h5>{data.testimonials[currentSlide].person}</h5>
-    //                 </div>
-    //                 <div className='slider-text__dots'>
-    //                     {data.testimonials.map((_, index) => (
-    //                         <button
-    //                             key={index}
-    //                             className={index === currentSlide ? 'active' : ''}
-    //                             onClick={() => setCurrentSlide(index)}
-    //                         ><span></span></button>
-    //                     ))}
-    //                 </div>
-    //             </div>
-    //         </section>
-    //     </div>
-    // );
 };
 
 export default Slider;
